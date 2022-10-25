23ed0edd27eee4a951ebf46fee09f4c232f687ba
Danila Fominykh <dfominykh@c2g.local>
2022-10-25 11:06:14 +0300

    added all packages that i need to install

---
+++ setup.sh
@@ -0,0 +1,64 @@
+# SETUP GIT
+git config --global user.email "dangdancheg@gmail.com"
+git config --global user.name "dancheg"
+
+# VSCODE
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/visual-studio-code-bin.git
+cd visual-studio-code-bin
+makepkg -si
+cd ..
+
+# GOOGLE CHROME
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+yay -S google-chrome
+cd ..
+
+# NODE
+pacman -S nodejs npm
+
+# FLUTTER
+snap install flutter --classic
+
+# GO
+sudo pacman -S go
+go install mvdan.cc/gofumpt@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1
+go install github.com/swaggo/swag/cmd/swag@latest
+source /etc/environment && export PATH=$(go env GOPATH)/bin:$PATH
+
+# FLATPACK
+sudo pacman -S flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+pacman -Sl multilib
+
+# STEAM
+sudo pacman -S steam
+
+# SNAP
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+# DISCORD
+sudo snap install discord
+
+# DOCKER&COMPOSE
+sudo pacman -S docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+pamac install docker-compose
+
+# TELEGRAM
+sudo snap install telegram-desktop
+
+# MATTERMOST
+sudo snap install mattermost-desktop
+
+# WIREGUARD
+sudo pacman -S wireguard-tools
+
+# GKSU
+sudo pacman -S gksu
